4cfe8293efe79e6576a3311fd89f76096087fc1a
Yanhao Zhu <yanhaozhu@gmail.com>
2013-02-24 11:20:36 -0500

    Changed the integer value for Sunday to 0, to match the value used in Time class.

--- lib/store_hours/constants.rb
+++ lib/store_hours/constants.rb
@@ -1,6 +1,6 @@
 module StoreHours
 
-  WEEKDAY_TO_NUM = { :mon => 1, :tue => 2, :wed => 3, :thu => 4, :fri => 5, :sat => 6, :sun => 7}
+  WEEKDAY_TO_NUM = { :mon => 1, :tue => 2, :wed => 3, :thu => 4, :fri => 5, :sat => 6, :sun => 0}
 
-  NUM_TO_WEEKDAY = { 1=> :Mon, 2 => :Tue, 3 => :Wed, 4 => :Thu, 5 => :Fri, 6 => :Sat, 7 => :Sun }
+  NUM_TO_WEEKDAY = { 1=> :Mon, 2 => :Tue, 3 => :Wed, 4 => :Thu, 5 => :Fri, 6 => :Sat, 0 => :Sun }
 end
